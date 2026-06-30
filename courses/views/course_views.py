c8c20e26f31922665cc97be65767c53965a11ee3
SriHarsha-17 <sriharshakakumanu1718@gmail.com>
2026-06-30 16:52:00 +0530

    Added Status codes in course_views.py

--- courses/views/course_views.py
+++ courses/views/course_views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import *
 from courses.models.course import Course
 from django.shortcuts import get_object_or_404
-from rest_framework.parsers import JSONParser
 from courses.serializers.course_serializer import CourseSerializer
 
 @api_view(['GET','POST'])
@@ -12,13 +12,14 @@ def course_list(request):
         courses = Course.objects.all()
         #response = {"courses" : list(courses.values())}
         serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_200_OK)
+    
     elif request.method == 'POST':
         serializer = CourseSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data,status=HTTP_201_CREATED)
+        return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
 def course_detail(request,pk):
@@ -26,13 +27,13 @@ def course_detail(request,pk):
     course = get_object_or_404(Course,pk=pk)
     if request.method == 'GET':
         serializer = CourseSerializer(course)
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_200_OK)
     elif request.method == 'PUT':
         serializer = CourseSerializer(course, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error)
+            return Response(serializer.data,status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         course.delete()
-        return Response({"status":"Course deleted sucessfully"})
+        return Response({"status":"Course deleted sucessfully"},status=HTTP_204_NO_CONTENT)
