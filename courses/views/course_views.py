62c973dc8ef9e26741c911b744c615890aa4efba
SriHarsha-17 <sriharshakakumanu1718@gmail.com>
2026-06-30 12:20:48 +0530

    Added POST,PUT and DELETE methods in course_views.py

--- courses/views/course_views.py
+++ courses/views/course_views.py
@@ -1,25 +1,38 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from courses.models.course import Course
 from django.shortcuts import get_object_or_404
 from rest_framework.parsers import JSONParser
 from courses.serializers.course_serializer import CourseSerializer
 
-
+@api_view(['GET','POST'])
 def course_list(request):
     if request.method == 'GET':
         courses = Course.objects.all()
         #response = {"courses" : list(courses.values())}
         serializer = CourseSerializer(courses, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CourseSerializer(data=data)
+        serializer = CourseSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors)
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
+@api_view(['GET','PUT','DELETE'])
 def course_detail(request,pk):
+
     course = get_object_or_404(Course,pk=pk)
-    serializer = CourseSerializer(course)
-    return JsonResponse(serializer.data)
+    if request.method == 'GET':
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CourseSerializer(course, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error)
+    elif request.method == 'DELETE':
+        course.delete()
+        return Response({"status":"Course deleted sucessfully"})
