c200f11850170655da8995762487aa9f643ffea4
SriHarsha-17 <sriharshakakumanu1718@gmail.com>
2026-06-30 11:52:30 +0530

    Update course views and ignore Python cache files

--- courses/views/course_views.py
+++ courses/views/course_views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse, JsonResponse
 from courses.models.course import Course
 from django.shortcuts import get_object_or_404
+from rest_framework.parsers import JSONParser
 from courses.serializers.course_serializer import CourseSerializer
 
+
 def course_list(request):
-    # serialization
-    courses = Course.objects.all()
-    #response = {"courses" : list(courses.values())}
-    serializer = CourseSerializer(courses, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        courses = Course.objects.all()
+        #response = {"courses" : list(courses.values())}
+        serializer = CourseSerializer(courses, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CourseSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors)
 
 def course_detail(request,pk):
     course = get_object_or_404(Course,pk=pk)
