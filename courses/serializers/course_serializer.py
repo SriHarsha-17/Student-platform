4cfc5b3c98cc678212dee90648df30dbba3a6ee8
SriHarsha-17 <sriharshakakumanu1718@gmail.com>
2026-07-01 07:41:39 +0530

    Added Validators for name and code in course_serialization.py

--- courses/serializers/course_serializer.py
+++ courses/serializers/course_serializer.py
@@ -1,13 +1,22 @@
 from rest_framework.serializers import *
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 from courses.models.course import Course
 
 class CourseSerializer(Serializer):
-    id = IntegerField()
-    name = CharField(max_length=100)
-    code = CharField()
-    #description
+    id = IntegerField(read_only=True)
+    name = CharField(max_length=100,validators =[UniqueValidator(queryset= Course.objects.all(),message="Course must be unique")])
+    code = CharField(validators =[UniqueValidator(queryset= Course.objects.all(),message="Code must be unique")])
     description = CharField(max_length=600)
 
+    # class Meta:
+    #     validators = [
+    #         UniqueTogetherValidator(
+    #             queryset = Course.objects.all(),
+    #             fields = ['name','code'],
+    #             message="Every course must have a unique name and code"
+    #         )
+    #     ]
+        
     def create(self, validated_data):
         """Create and re turn a new `Course` instance, given the validated data."""
         return Course.objects.create(**validated_data)
